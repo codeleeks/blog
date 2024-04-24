f7ebebe8964825b827451a9a2899f92bbbd3f1e3
codeleeks <kasonglee@gmail.com>
2024-04-24 12:11:04 +0900

    포스트에 타이틀 이미지 추가

--- src/components/Post/PostContents.jsx
+++ src/components/Post/PostContents.jsx
@@ -2,14 +2,16 @@ import Markdown from 'react-markdown'
 import rehypeHighlight from 'rehype-highlight'
 import rehypeSlug from 'rehype-slug'
 import rehypeRaw from 'rehype-raw'
-import { skipMetadata } from '../../utils/post'
+import { extractTitleImage, skipMetadata } from '../../utils/post'
 
 export default (props) => {
   const { title, contents } = props
 
+  const titleImg = extractTitleImage(contents)
   return (
     <>
       <h1 className='title'>{title}</h1>
+      <img src={titleImg} alt={title} className='title-image'/>
       <Markdown
         rehypePlugins={[rehypeHighlight, rehypeRaw, rehypeSlug]}
         className='contents'
