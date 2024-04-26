9d93779f23e1b25c965b8d4c26c34a6303f80ab0
codeleeks <kasonglee@gmail.com>
2024-04-26 15:25:39 +0900

    프롤로그 수정 완료.

--- src/components/Prologue/Hero.jsx
+++ src/components/Prologue/Hero.jsx
@@ -1,18 +1,29 @@
+import { SKILLS } from '../../data/skills'
+import selfiImg from '../../assets/행복한나.jpg'
 export default (props) => {
   return (
     <figure>
-      <ul className='canvas'>
-        {[...Array(200)].map((_, i) => (
-          <li key={i} className='star'></li>
-        ))}
-      </ul>
+      <img src={selfiImg} alt='selfi' />
       <figcaption>
-        <h2>KASONG LEE</h2>
-        <blockquote>
-          Be <span className='happy'>happy</span> <br />
-          <span className='healthy'>healthy</span> <br />
-          <span className='realistic'>realistic.</span>
-        </blockquote>
+        <h1>
+          KASONG <br />
+          <span>LEE</span>
+        </h1>
+        <div className='brief'>
+          <p>Samsung Electronics Software Engineer.</p>
+          <div className='tags'>
+            <span>#backend</span>
+            <span>#frontend</span>
+            <span>#cloud</span>
+          </div>
+        </div>
+        <ul className='skills'>
+          {SKILLS.map((skill) => (
+            <li key={skill.name}>
+              <img src={skill.img} alt={skill.name} />
+            </li>
+          ))}
+        </ul>
       </figcaption>
     </figure>
   )
