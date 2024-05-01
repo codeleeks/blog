0081e45d0bf679656fa43d163b93de57bf998ae9
codeleeks <kasonglee@gmail.com>
2024-05-01 14:58:44 +0900

    table of contents 반응형 UI 적용

--- src/components/Post/TableOfContents.jsx
+++ src/components/Post/TableOfContents.jsx
@@ -1,4 +1,30 @@
+import { useState } from 'react'
+import Icon from '../UI/Icon'
+
 export default (props) => {
   const { children } = props
-  return <aside>{children}</aside>
+  const [isOpen, setIsOpen] = useState()
+
+  const toggleHandler = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen)
+    document.body.classList.toggle('toc--open')
+  }
+
+  const closeHandler = () => {
+    console.log('clicked')
+    setIsOpen(false)
+    document.body.classList.remove('toc--open')
+  }
+
+  return (
+    <aside className={isOpen ? 'toc--open' : ''}>
+      <div className='toc-bg' onClick={closeHandler}></div>
+      <div className='toc-toggler' onClick={toggleHandler}>
+        <Icon>segment</Icon>
+      </div>
+      <div className='toc__scroll-area'>
+        <div className='toc__contents-area'>{children}</div>
+      </div>
+    </aside>
+  )
 }
